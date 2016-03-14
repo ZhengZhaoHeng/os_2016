3bc62abc7dd3377d2ba5c6f5a0a6a3491c840721
sola777 <zhanghaotian1994@163.com>
2016-03-14 16:34:59 +0800

    lec7

--- lec7/page_table.cpp
+++ lec7/page_table.cpp
@@ -9,7 +9,7 @@ void make_table(unsigned int va, unsigned int pa)
 
 	unsigned int pte_idx = (va >> 12) & 0x3ff;
 
-	unsigned int pte_ctx = pa & ~0x3FF | 0x3;
+	unsigned int pte_ctx = (pa & 0xFFFFF000) | 0x3;
 
 	printf("va 0x%08x, pa 0x%08x\npde_idx 0x%08x, pde_ctx 0x%08x, pte_idx 0x%08x, pte_ctx 0x%08x\n", va, pa, pde_idx, pde_ctx, pte_idx, pte_ctx);
 }
