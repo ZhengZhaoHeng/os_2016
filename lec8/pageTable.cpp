30b32146c2bb0e0821c1410f2618225ff132a635
sola777 <zhanghaotian1994@163.com>
2016-03-16 17:10:09 +0800

    lec8

--- lec8/pageTable.cpp
+++ lec8/pageTable.cpp
@@ -16,35 +16,68 @@ void lookup_disk(short vaddress)
 
 	short valid0 = pde_c >> 7;
 	short pfn0 = pde_c & 0x7f;
-	cout<<"--> pde index : 0x"<< hex <<pde_i<<" pde contents:( valid "<< valid0 <<", pfn 0x"<< hex <<pfn0<<" )"<<endl;
+	cout<<"--> pde index : 0x"<< hex <<pde_i<<" pde contents:(0x"<<pde_c<<", valid "<< valid0 <<", pfn 0x"<< hex <<pfn0<<" )"<<endl;
+	
+	cout<<"page 6c: ";
+	for(int i=0; i<16; i++)
+		cout<<memory[0x6c][i]<<' ';
+	cout<<endl<<"         ";
+	for(int i=16; i<32; i++)
+		cout<<memory[0x6c][i]<<' ';
+	cout<<endl;
+	
 	if(valid0 == 0)
 		cout<<"    --> Fault (page directory entry not valid)"<<endl;
 	else
 	{
+		cout<<"page "<<hex<<pfn0<<": ";
+		for(int i=0; i<16; i++)
+			cout<<memory[pfn0][i]<<' ';
+		cout<<endl<<"         ";
+		for(int i=16; i<32; i++)
+			cout<<memory[pfn0][i]<<' ';
+		cout<<endl;
+
 		short pte_i = (vaddress >> 5) & 0x1f;
 		short pte_c = memory[pfn0][pte_i];
 		short valid1 = pte_c >> 7;
 		short pfn1 = pte_c & 0x7f;
-		cout<<"    --> pte index : 0x"<< hex<<pte_i <<" pte contents: ( valid "<<valid1 <<", pfn 0x"<< hex <<pfn1<<" )"<<endl;
+		cout<<"    --> pte index : 0x"<< hex<<pte_i <<" pte contents:(0x"<<pte_c<<", valid "<<valid1 <<", pfn 0x"<< hex <<pfn1<<" )"<<endl;
 
 		if(valid1 == 0)
 		{
 			if(pfn1 == 0x7f)
 			{
-				cout<<"        --> Neither in memory, nor in disk"<<endl;
+				cout<<"        --> neither in memory, nor in disk"<<endl;
 			}
 			else
 			{
+				cout<<"page "<<hex<<pfn1<<": ";
+				for(int i=0; i<16; i++)
+					cout<<disk[pfn1][i]<<' ';
+				cout<<endl<<"         ";
+				for(int i=16; i<32; i++)
+					cout<<disk[pfn1][i]<<' ';
+				cout<<endl;
+
 				short diskaddress = (pfn1 << 5) + (vaddress & 0x001f);
 				short value = disk[pfn1][vaddress & 0x001f];
-				cout<<"        --> To Disk Sector Address"<< hex << diskaddress <<" --> Value: "<< hex <<value<<endl;
+				cout<<"        --> To Disk Sector Address 0x"<< hex << diskaddress <<" --> Value: "<< hex <<value<<endl;
 			}
 		}
 		else
 		{
+			cout<<"page "<<hex<<pfn1<<": ";
+			for(int i=0; i<16; i++)
+				cout<<memory[pfn1][i]<<' ';
+			cout<<endl<<"         ";
+			for(int i=16; i<32; i++)
+				cout<<memory[pfn1][i]<<' ';
+			cout<<endl;
+
 			short paddress = (pfn1 << 5) + (vaddress & 0x001f);
 			short value = memory[pfn1][vaddress & 0x001f];
-			cout<<"        --> To Physical Address "<< hex <<paddress<<" --> Value: "<<hex<<value<<endl;
+			cout<<"        --> To Physical Address 0x"<< hex <<paddress<<" --> Value: "<<hex<<value<<endl;
 		}
 	}
 }
@@ -80,6 +113,6 @@ void init()
 int main()
 {
 	init();
-	lookup_disk(0x0af6);
+	lookup_disk(0x0330);
 	return 0;
 }
