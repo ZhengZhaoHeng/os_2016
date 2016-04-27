b0ee5e33605731f0dae6478760df1570b9f947dc
sola <zhanghaotian1994@163.com>
2016-04-27 19:50:40 +0800

    lec18

--- lec18/monitor.cpp
+++ lec18/monitor.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-#define MAX_SEATS 100
+#define MAX_SEATS 4
 #define MAX_ACTION 100
 
 struct ReadingRoom
@@ -15,6 +15,7 @@ struct ReadingRoom
 	pthread_mutex_t lock;
 	pthread_cond_t notFull, notEmpty;
 	int seats;
+	int waits;
 	ReadingRoom();
 };
 
@@ -24,6 +25,7 @@ ReadingRoom::ReadingRoom()
 	notEmpty = PTHREAD_COND_INITIALIZER;
 	notFull = PTHREAD_COND_INITIALIZER;
 	seats = MAX_SEATS;
+	waits = 0;
 }
 
 void* enter(void *arg)
@@ -31,23 +33,43 @@ void* enter(void *arg)
 	ReadingRoom *room = (ReadingRoom*)arg; 
 	pthread_mutex_lock(&room->lock);
 	while(room->seats == 0)
+	{
+		room->waits ++;
+		cout<<"One reader waits in the reading room!"<<endl;
+		cout<<"Current Status:"<<endl;
+		cout<<"seats: "<<room->seats<<endl;
+		cout<<"readers in the room: "<<MAX_SEATS-room->seats<<endl;
+		cout<<"readers waiting "<<room->waits<<endl<<endl;
 		pthread_cond_wait(&room->notFull, &room->lock);
+		room->waits --;
+	}
 	room->seats --;
 	pthread_cond_signal(&room->notEmpty);
-	pthread_mutex_unlock(&room->lock);
 	cout<<"One reader enters the reading room!"<<endl;
+	cout<<"Current Status:"<<endl;
+	cout<<"seats: "<<room->seats<<endl;
+	cout<<"readers in the room: "<<MAX_SEATS-room->seats<<endl;
+	cout<<"readers waiting "<<room->waits<<endl<<endl;
+	pthread_mutex_unlock(&room->lock);
 }
 
 void* leave(void *arg)
 {
 	ReadingRoom *room = (ReadingRoom*)arg; 
 	pthread_mutex_lock(&room->lock);
-	while(room->seats == MAX_SEATS)
-		pthread_cond_wait(&room->notEmpty, &room->lock);
+	if(room->seats == MAX_SEATS)
+	{
+		pthread_mutex_unlock(&room->lock);
+		return 0;
+	}
 	room->seats ++;
 	pthread_cond_signal(&room->notFull);
-	pthread_mutex_unlock(&room->lock);
-	cout<<"One reader leaves the reading room!"<<endl;	
+	cout<<"One reader leaves the reading room!"<<endl;
+	cout<<"Current Status:"<<endl;
+	cout<<"seats: "<<room->seats<<endl;
+	cout<<"readers in the room: "<<MAX_SEATS-room->seats<<endl;
+	cout<<"readers waiting "<<room->waits<<endl<<endl;
+	pthread_mutex_unlock(&room->lock);	
 }
 
 int main()
